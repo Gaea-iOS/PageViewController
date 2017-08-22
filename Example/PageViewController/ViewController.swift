d32773ee35008304718f68b77970dc702dba6efb
jerry <445242970@qq.com>
2017-08-22 10:56:17 +0800

    first commit

--- Example/PageViewController/ViewController.swift
+++ Example/PageViewController/ViewController.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import PageViewController
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    private lazy var pageViewController: PageViewController = { [unowned self] in
+    
+        let controller1 = self.controller(with: .red)
+        let controller2 = self.controller(with: .yellow)
+        let controller3 = self.controller(with: .blue)
+        let controller4 = self.controller(with: .purple)
+
+        let controller = PageViewController(controllers: [controller1, controller2, controller3, controller4], interPageSpacing: 10)
+        return controller
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addChildViewController(pageViewController)
+        containerView.addSubview(pageViewController.view)
+        pageViewController.view.frame = containerView.bounds
+        
+        
+        pageViewController.didScrollToIndex = { index in
+            print("didScrollToIndex = \(index)")
+
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func controller(with color: UIColor) -> UIViewController {
+    
+        let controller1 = UIViewController()
+        controller1.view.backgroundColor = color
+        return controller1
+    }
+    
+    @IBAction func clickButton(sender: Any) {
+        
+        pageViewController.removeCurrentController()
+        print("totalPages = \(pageViewController.totalPages)")
+        print("current index = \(pageViewController.currentIndex)")
     }
 
 }
